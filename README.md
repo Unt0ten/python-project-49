c7de8fead21e942d4d41cfaffea0ade424fb3c00
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-04-26 18:48:09 +0700

    add step 5 relise

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Unt0ten/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/Unt0ten/python-project-49/actions)
+
+https://asciinema.org/a/6dDvFd8AGdtVDzQFksfmojNbi
--- brain_games/parity_check.py
+++ brain_games/parity_check.py
@@ -8,18 +8,16 @@ def welcome_user():
     print(f'Hello {name}!')
     return name
 
-name = welcome_user()
 
 # function to determine the parity of a random number
 def parity_check_game():
+    name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     for _ in range(3):  # number of attempts
         random_number = randint(1, 20)
         print(f'Question: {random_number}')  # question asked to the user
-        global user_response
         user_response = input()  # user enters a response
         # determine the correct answer
-        global correct_answer
         if random_number % 2 == 0:
             correct_answer = 'yes'
         else:
@@ -28,10 +26,10 @@ def parity_check_game():
             break
         else:
             print('Correct!')
-    end_game()
-    return user_response, correct_answer
+    end_game(user_response, correct_answer, name)
 
-def end_game():
+
+def end_game(user_response, correct_answer, name):
     if user_response != correct_answer:
         print(f"'{user_response}' is wrong answer ;(. Correct answer was '{correct_answer}'. Let's try again, {name}!")
     else:
