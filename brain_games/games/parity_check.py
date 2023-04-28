50c9a696136ed934e2539dfb516a25585a43dae0
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-04-28 18:22:16 +0700

    parity_check modified, deleted game_functionality

--- brain_games/games/parity_check.py
+++ brain_games/games/parity_check.py
@@ -2,7 +2,15 @@ from brain_games.game_logic import game_logic
 from random import randint
 
 
+def question():
+    random_number = randint(1, 20)
+    return random_number
+
+
+def correct_answer(question):
+    correct_answer = 'yes' if question() % 2 == 0 else 'no'
+    return correct_answer
+
 game_conditions = 'Answer "yes" if the number is even, otherwise answer "no".'
-answer = randint(1, 20)
-correct_answer = 'yes' if answer % 2 == 0 else 'no'
-game_logic(game_conditions, answer, correct_answer)
+
+game_logic(game_conditions, question, correct_answer)
