d6a30d71b52481331f0a152c07c94df644a82e17
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-04-29 21:12:03 +0700

    step 6

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Actions Status](https://github.com/Unt0ten/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/Unt0ten/python-project-49/actions)
 
 https://asciinema.org/a/6dDvFd8AGdtVDzQFksfmojNbi
+
+https://asciinema.org/a/ALde2VFQxPLPyaeAq6qfcRz0t
--- brain_games/games/brain_calc_game.py
+++ brain_games/games/brain_calc_game.py
@@ -9,19 +9,21 @@ def game_conditions():
 
 def question_generator():
     first_random_number = randint(1, 20)
-    second_random_number = randint(1, 20)
+    second_random_number = randint(1, 10)
     operator = choice(['-', '+', '*'])
-    question = f'{first_random_number} {operator} {second_random_number}'
-    return question
+    answer = f'{first_random_number} {operator} {second_random_number}'
+    return answer
 
 
-def correct_answer(question):
-    if question in '-':
-        correct_answer = first_random_number - second_random_number
-    elif question in '+:':
-        correct_answer = first_random_number + second_random_number
-    elif question in '*':
-        correct_answer = first_random_number * second_random_number
-    return correct_answer
+def correct_answer(answer):
+    answer = answer.split(' ')
+    if '-' in answer[1]:
+        correct_answer = int(answer[0]) - int(answer[2])
+    elif '+' in answer[1]:
+        correct_answer = int(answer[0]) + int(answer[2])
+    elif '*' in answer[1]:
+        correct_answer = int(answer[0]) * int(answer[2])
+    return str(correct_answer)
+
 
 game_logic(game_conditions, question_generator, correct_answer)
